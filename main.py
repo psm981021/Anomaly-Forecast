1744a97ae1cb2860f870b4ae8a158168c0b0b59c
seongbeom <psm981021@naver.com>
2024-04-16 16:26:21 +0900

    dataloader -ing

--- main.py
+++ main.py
@@ -16,3 +16,9 @@ def main():
     parser.add_argument("--epochs", type=int, help="number of epochs" )
     parser.add_argument("--model_idx", default="test", type=str, help="model idenfier")
     parser.add_argument("--gpu_id", type=str, default="0", help="gpu_id")
+    parser.add_argument("--learning_rate", type=float, default=0.001, help="learning rate")
+    parser.add_argument("--batch", type=int, help="batch size")
+    parser.add_argument("--device",type=str, default="cuda:1")
+    parser.add_argument("--seed",type=int, default="42")
+
+
---
+++ utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import random
+import os
+import torch
+
+def set_seed(seed):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    # some cudnn methods can be random even after fixing the seed
+    # unless you tell it to be deterministic
+    torch.backends.cudnn.deterministic = True
