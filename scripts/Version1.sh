de28c15d62365e2b2fe80b218a4daa409a722455
woney23 <jiwonb683@gmail.com>
2024-05-16 21:12:16 +0900

    jw scripts change

--- scripts/Version1.sh
+++ scripts/Version1.sh
@@ -6,3 +6,6 @@ python main.py \
     --test_list []
 
 #--use_multi_gpu \ma
+
+
+python main.py --data_dir "data/radar_test" --image_csv_dir "data\\22.7_22.9 강수량 평균 0.1 이하 제거_set추가.csv" --batch 8 --model_idx "test-projection" --test_list []
