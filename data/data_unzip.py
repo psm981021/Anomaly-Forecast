cbd656259abd8d2937c15722d7ce5ffe43e7f776
seongbeom <psm981021@naver.com>
2024-05-16 20:06:30 +0900

    5/16 extract images from original image

--- data/data_unzip.py
+++ data/data_unzip.py
@@ -1,4 +1,30 @@
 import zipfile
+import pandas as pd
+import os
+import shutil
+
+def extract_image_file(csv_path,raw_data_path, extract_path):
+    dataframe = pd.read_csv(csv_path)
+
+    data_list = []
+    path_columns = ['t-60','t-50','t-40','t-30','t-20','t-10','t']
+
+    for index, row in dataframe.iterrows():
+        for column in path_columns:
+            full_path = os.path.join(raw_data_path, row[column])
+            data_list.append(full_path)
+    
+    
+    # make a directory if not exists
+    if not os.path.exists(extract_path):
+        os.makedirs(extract_path)
+
+    for file_path in data_list:
+        if os.path.isfile(file_path):  # Check if the file exists
+            # Copy the file to the new directory
+            shutil.copy(file_path, extract_path)
+        else:
+            print(f"File not found: {file_path}")  
 
 if __name__ == "__main__":
 
@@ -7,3 +33,4 @@ if __name__ == "__main__":
 
     with zipfile.ZipFile(path_to_zipfile, 'r') as zip_ref:
         zip_ref.extractall(directory_to_unzip)
+
