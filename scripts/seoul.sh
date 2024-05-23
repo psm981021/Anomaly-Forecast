6f2e4103809f6add71e5e0a92667bd57a3f544fb
seongbeom <psm981021@naver.com>
2024-05-23 20:16:58 +0900

    shell add

--- scripts/seoul.sh
+++ scripts/seoul.sh
@@ -5,16 +5,31 @@ python main.py \
     --batch 8 \
     --epochs 1000 \
     --patience 50 \
-    --model_idx "seoul-V.2.01" \
-    --output_dir "output/seoul-V.2.01/" \
+    --model_idx "seoul-V.3.01" \
+    --output_dir "output/seoul-V.3.01/" \
     --test_list=[] \
     --gpu_id 0 \
     --device cuda:0 \
     --seed 0 \
     --grey_scale \
-    --loss_type "ed_image" \
+    --loss_type "stamina" \
     --wandb
 
+python main.py \
+    --data_dir "data/data_seoul_only" \
+    --image_csv_dir "data/서울_2021_2023_강수량 0.1 미만 제거_상위 10% test.csv" \
+    --batch 8 \
+    --epochs 1000 \
+    --patience 50 \
+    --model_idx "seoul-V.2.02" \
+    --output_dir "output/seoul-V.2.02/" \
+    --test_list=[] \
+    --gpu_id 0 \
+    --device cuda:0 \
+    --seed 0 \
+    --grey_scale \
+    --loss_type "stamina" \
+    --wandb
 # scripts/seoul.sh
 
 # minjoo computer
