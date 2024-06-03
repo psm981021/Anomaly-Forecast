35c32e257482f1b736a91c211783cf656849eff7
woney23 <jiwonb683@gmail.com>
2024-06-03 09:51:38 +0000

    jw data_unzip update

--- data/data_unzip.py
+++ data/data_unzip.py
@@ -29,7 +29,7 @@ def extract_image_file(csv_path,raw_data_path, extract_path):
 if __name__ == "__main__":
 
     path_to_zipfile = 'data/radar_full.zip'
-    directory_to_unzip = 'data/radar_full'
+    directory_to_unzip = 'data/'
 
     with zipfile.ZipFile(path_to_zipfile, 'r') as zip_ref:
         zip_ref.extractall(directory_to_unzip)
