3b05475ec142aef0144a70fde2d13d118a40ab7b
woney23 <jiwonb683@gmail.com>
2024-06-03 15:32:45 +0000

    jw data gpu match

--- scripts/seoul_st.sh
+++ scripts/seoul_st.sh
@@ -1,22 +1,38 @@
 python main.py \
     --data_dir "/dev/shm/data_seoul_only_v2" \
-    --image_csv_dir "data/seoul_v2.csv" \
+    --image_csv_dir "data/Seoul.csv" \
     --batch 8 \
     --epochs 3000 \
     --patience 40 \
-    --model_idx "seoul-L4.03" \
-    --output_dir "output/seoul-L4.03/" \
+    --model_idx "test2" \
+    --output_dir "output/test2/" \
     --test_list=[] \
     --gpu_id 1 \
     --device cuda:1 \
     --seed 0 \
-    --lr 0.01 \
+    --lr 0.001 \
     --loss_type "stamina" \
     --location "seoul" \
     --regression "gap" \
-    --classification
+    --pre_train
 
 
+python main.py \
+    --data_dir "/dev/shm/data_seoul_only_v2" \
+    --image_csv_dir "data/Seoul.csv" \
+    --batch 8 \
+    --epochs 3000 \
+    --patience 40 \
+    --model_idx "test2" \
+    --output_dir "output/test2/" \
+    --test_list=[] \
+    --gpu_id 1 \
+    --device cuda:1 \
+    --seed 0 \
+    --lr 0.02 \
+    --loss_type "stamina" \
+    --location "seoul" \
+    --regression "gap" \
 
 
 # scripts/seoul_st.sh
